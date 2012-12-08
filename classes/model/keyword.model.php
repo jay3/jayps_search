8b1ce21805df3a44604390eb3b6729d332360423
JayPS <dev@jayps.fr>
2012-12-09 00:32:37 +0100

    Define $_primary_key

--- classes/model/keyword.model.php
+++ classes/model/keyword.model.php
@@ -5,4 +5,5 @@ namespace JayPS\Search;
 class Model_Keyword extends \Nos\Orm\Model
 {
     protected static $_table_name = 'jayps_search_word_occurence';
+    protected static $_primary_key = array();
 }
