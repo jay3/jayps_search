96a3841dcb3dacb8c58db03746f49289f97bf5b3
JayPS <dev@jayps.fr>
2014-05-14 09:42:21 +0200

    Bugfix: prevent corrupting the table with foreign_id=0
    This could happen before when deleting an indexed model (because mooc_join_table is not part of the key in the relation).
    If you have corrupted values in jayps_search_word_occurence, you should reindex the full table.

--- classes/model/keyword.model.php
+++ classes/model/keyword.model.php
@@ -6,4 +6,15 @@ class Model_Keyword extends \Nos\Orm\Model
 {
     protected static $_table_name = 'jayps_search_word_occurence';
     protected static $_primary_key = array('mooc_word');
+
+    public function save($cascade = null, $use_transaction = false)
+    {
+        // Example: called when the indexed model is deleted (through the 'has_many' relation)
+        // NOTE: after_save & before_delete should perform any required operations on the table jayps_search_word_occurence,
+        // there is no need to do anything here
+
+        // return WITHOUT calling the parent
+        // (this would corrupt mooc_foreign_id with 0 in the full table because mooc_join_table is not part of the key in the relation)
+        return;
+    }
 }
